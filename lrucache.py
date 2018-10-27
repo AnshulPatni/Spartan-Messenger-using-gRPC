2a498fdd931521f766627a4caee3b7e8be217364
AnshulPatni <anshulpatni19@gmail.com>
2018-10-26 19:27:37 -0700

    LRU Cache implemented

---
+++ lrucache.py
@@ -0,0 +1,28 @@
+import collections
+
+import time
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
+
+    def get(self, key):
+        try:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        except KeyError:
+            return -1
+
+    def set(self, key, value):
+        try:
+            self.cache.pop(key)
+        except KeyError:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)
+        self.cache[key] = value
+
+    def show(self):
+        # messages = list(self.cache.values())
+        return(list(self.cache.values()))
