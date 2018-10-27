db4311cc8be06d072a0d9801be059db42a7fc02c
AnshulPatni <anshulpatni19@gmail.com>
2018-10-26 19:28:37 -0700

    Added AES encryption and LRU Cache with time limit

--- client.py
+++ client.py
@@ -1,69 +1,150 @@
 import threading
-from tkinter import *
-from tkinter import simpledialog
-
+import sys
+from time import sleep
+import re
+import yaml
+from Crypto.Cipher import AES
 import grpc
 
-import spartanmessenger_pb2 as chat
-import spartanmessenger_pb2_grpc as rpc
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+import messenger_pb2 as chat;
+import messenger_pb2_grpc as rpc;
 
 address = 'localhost'
-port = 11912
 
+name = ''
+othername = ''
+
+with open("config.yaml", 'r') as stream:
+    try:
+        yaml_file = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+port = yaml_file['port']
 
 class Client:
 
-    def __init__(self, u: str, window):
-        # the frame to put ui components on
-        self.window = window
-        self.username = u
-        # create a gRPC channel + stub
+    def __init__(self, u: str, user_list):
+        self.name = u
+       
         channel = grpc.insecure_channel(address + ':' + str(port))
         self.conn = rpc.ChatServerStub(channel)
-        # create new listening thread for when new message streams come in
+
+        nUsr = chat.User()
+        nUsr.name = u
+        nRetUsr = chat.User()
+        nRetUsr = self.conn.CheckMessageList(nUsr)
+
+        if(nRetUsr.name != "NA"):
+            askToChat = input(nRetUsr.name + " is requesting to chat with you. Enter 'yes' to accept or different user: ")
+            if(askToChat == "yes"):
+                o = nRetUsr.name
+            else:
+                o = askToChat
+        else:
+            o = input("[Spartan] Enter a user whom you want to chat with: ")
+
+        if o not in user_list:
+            print(o + " is not allowed to use the messenger.")
+            print("Try again with one of the following users: " + ','.join(str(x) for x in user_list))
+            return
+
+        print("[Spartan] You are now ready to chat with " + o + ".")
+
+        n = chat.User()
+        n.name = u
+        n.othername = o
+        self.othername = o
+        if(not(self.conn.CreateConnection(n).successful)):
+            print("[Spartan] Connection failed!!")
         threading.Thread(target=self.__listen_for_messages, daemon=True).start()
-        self.__setup_ui()
-        self.window.mainloop()
+        self.send_message()
+    
+    def encrypt_message(self, message):
+        byte_message = message.encode()
+        key = b"Sixteen byte key"
+
+        cipher = AES.new(key, AES.MODE_EAX)
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(byte_message)
+        file_out = open("encrypted.bin", "wb")
+        [ file_out.write(x) for x in (nonce, tag, ciphertext) ]
+        return(nonce, ciphertext, tag)
+
+    def decrypt_message(self, nonce, ciphertext, tag):
+        key = b'Sixteen byte key'
+        
+        cipher = AES.new(key, AES.MODE_EAX, nonce)
+        byte_message = cipher.decrypt_and_verify(ciphertext, tag)
+        
+        message = byte_message.decode()
+        
+        return(message)
 
     def __listen_for_messages(self):
-        """
-        This method will be ran in a separate thread as the main/ui thread, because the for-in call is blocking
-        when waiting for new messages
-        """
-        for note in self.conn.ChatStream(chat.Empty()):
-            print("R[{}] {}".format(note.name, note.message))
-            self.chat_list.insert(END, "[{}] {}\n".format(note.name, note.message))
 
-    def send_message(self, event):
-        """
-        This method is called when user enters something into the textbox
-        """
-        message = self.entry_message.get()
-        if message is not '':
-            n = chat.Note()
-            n.name = self.username
-            n.message = message
-            print("S[{}] {}".format(n.name, n.message))
-            self.conn.SendNote(n)
+        nl = chat.User()
+        nl.name = self.name
+        nl.othername = self.othername
+        def findWholeWord(w, s):
+            return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search(s)
+        while True:
+            for note in self.conn.ChatStream(nl):
+                message = self.decrypt_message(note.nonce, note.ciphertext, note.tag)
+                if(findWholeWord(nl.name, message) == None):
+                    print(message)
+    
+    def send_message(self):
 
-    def __setup_ui(self):
-        self.chat_list = Text()
-        self.chat_list.pack(side=TOP)
-        self.lbl_username = Label(self.window, text=self.username)
-        self.lbl_username.pack(side=LEFT)
-        self.entry_message = Entry(self.window, bd=5)
-        self.entry_message.bind('<Return>', self.send_message)
-        self.entry_message.focus()
-        self.entry_message.pack(side=BOTTOM)
+        n1 = chat.WarningMsg
+        try:
+            while True:
+                msg = input("[" + self.name + "] > ")
+                message = "[" + self.name + "] " + msg
+                n = chat.Msg()
+                n.name = self.name
+                n.othername = self.othername
 
+                encrypted_msg = self.encrypt_message(message)
+                n.nonce = encrypted_msg[0]
+                n.ciphertext = encrypted_msg[1]
+                n.tag = encrypted_msg[2]
 
+                n1 = self.conn.SendMessage(n)
+                for w in n1:
+                    if(w.warning != ""):
+                        print(w.warning)
+        except KeyboardInterrupt:
+            print("\nBye {}".format(name))
+            
 if __name__ == '__main__':
-    root = Tk()
-    frame = Frame(root, width=300, height=300)
-    frame.pack()
-    root.withdraw()
-    username = None
-    while username is None:
-        username = simpledialog.askstring("Username", "What's your username?", parent=root)
-    root.deiconify()
-    c = Client(username, frame)
+    name = ""
+    try:
+        print('[Spartan] Connected to Spartan Server at port {}.'.format(port))
+        if len(sys.argv) != 2:
+            name = input("[Spartan] What's your name?: ")
+        else:
+            name = sys.argv[1]
+        yaml_file = {}
+        with open("config.yaml", 'r') as stream:
+            try:
+                yaml_file = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        users = yaml_file['users']
+
+        if name in users:
+            user_list = users
+            user_list.remove(name)
+
+            print("[Spartan] User list: " + ','.join(str(x) for x in user_list))
+
+            c = Client(name, user_list)
+        else:
+            print(name + " is not allowed to use the messenger.")
+            print("Try again with one of the following users: " + ','.join(str(x) for x in users))
+    except KeyboardInterrupt:
+            print("\nBye {}".format(name))
--- server.py
+++ server.py
@@ -1,58 +1,149 @@
-from concurrent import futures
+from concurrent import futures;
+import grpc;
+import time;
 
-import grpc
-import time
+import messenger_pb2 as chat;
+import messenger_pb2_grpc as rpc;
+import yaml;
+import lrucache;
 
-import spartanmessenger_pb2 as chat
-import spartanmessenger_pb2_grpc as rpc
+with open("config.yaml", 'r') as stream:
+    try:
+        yaml_file = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 
+port = yaml_file['port']
+max_num_messages = yaml_file['max_num_messages_per_user']
+rate_limit = yaml_file['max_call_per_30_seconds_per_user']
+users = yaml_file['users']
+
+rate_limit_time = 30
 
 class ChatServer(rpc.ChatServerServicer):
 
     def __init__(self):
-        # List with all the chat history
-        self.chats = []
 
-    # The stream which will be used to send new messages to clients
-    def ChatStream(self, request_iterator, context):
-        """
-        This is a response-stream type call. This means the server can keep sending messages
-        Every client opens this connection and waits for server to send new messages
-        :param request_iterator:
-        :param context:
-        :return:
-        """5
-        lastindex = 0
-        # For every client a infinite loop starts (in gRPC's own managed thread)
+        self.cache = {}
+        self.messages = {}
+        self.unreadMessages =  {}
+        self.registeredUsers = []
+        self.sent_num_messages = {}
+        self.requestFromUser = {}
+        for usr in yaml_file['users']:
+            self.registeredUsers.append(chat.User(name=usr))
+            self.unreadMessages[usr] = {}
+            self.sent_num_messages[usr] = []
+
+    def CreateConnection(self, request: chat.User, context):
+        users = [request.name, request.othername]
+        self.requestFromUser[request.othername] = chat.User(name=request.name)
+        users.sort()
+        new_key = users[0] + "-" + users[1]
+        
+        for online_user in self.registeredUsers:
+            if (online_user.name == request.othername):
+                if new_key not in self.messages:
+                    self.messages[new_key] = lrucache.LRUCache(max_num_messages)
+                return chat.Connection(successful=True)
+        return chat.Connection(successful=False)
+
+    def CheckMessageList(self, request, context):
+        if request.name in self.requestFromUser:
+            return self.requestFromUser[request.name]
+        else:
+            return chat.User(name="NA")
+
+    def rateLimit(self, user, current_time):
+
+        num_messages = len(self.sent_num_messages[user])
+
+        if num_messages == 0:
+            self.sent_num_messages[user].append(current_time)
+            return True
+        
+        for i in range(0, num_messages):
+            time_interval = current_time - self.sent_num_messages[user][i]
+            
+            if num_messages == rate_limit:
+                if time_interval <= rate_limit_time:
+                    return False
+                else:
+                    i = i + 1
+            else:
+                if time_interval > rate_limit_time:
+                    i = i + 1
+                else:
+                    break
+
+        self.sent_num_messages[user] = self.sent_num_messages[user][i:]
+        self.sent_num_messages[user].append(current_time)
+        return True
+
+    def SendMessage(self, request: chat.Msg, context):
+
+        current_time = time.time()
+        
+        users = [request.name, request.othername]
+        sorted_users = sorted(users)
+        dict_key = sorted_users[0] + '-' + sorted_users[1]
+
+        isUnderLimit = self.rateLimit(users[0], current_time)
+
+        if isUnderLimit:
+            self.messages[dict_key].set(current_time, (request))
+            if self.unreadMessages[users[0]][users[1]] < max_num_messages:
+                self.unreadMessages[users[0]][users[1]] += 1
+            if self.unreadMessages[users[1]][users[0]] < max_num_messages:
+                self.unreadMessages[users[1]][users[0]] += 1
+            yield(chat.WarningMsg(warning=""))
+        else:
+            yield(chat.WarningMsg(warning="[Spartan] WARNING: You cannot send more messages. You have crossed the rate limit."))
+    
+    def ChatStream(self, request: chat.User, context):
+
+        n = chat.Msg()
+        users = [request.name, request.othername]
+        sorted_users = sorted(users)
+        my_key = sorted_users[0] + '-' + sorted_users[1]
+        key2 = users[0] + '-' + users[1]
+        n.name = users[0]
+        n.othername = users[1]
+
+        sender = users[0]
+        receiver = users[1]
+
+        #using lru cache to stream new messages
+
         while True:
-            # Check if there are any new messages
-            while len(self.chats) > lastindex:
-                n = self.chats[lastindex]
-                lastindex += 1
-                yield n
 
-    def SendNote(self, request: chat.Note, context):
-        """
-        This method is called when a clients sends a Note to the server.
-        :param request:
-        :param context:
-        :return:
-        """
-        print("[{}] {}".format(request.name, request.message))
-        # Add it to the chat history
-        self.chats.append(request)
-        return chat.Empty()
+            if self.unreadMessages[sender].get(receiver) == None:
+                self.unreadMessages[sender][receiver] = 0
+            if self.unreadMessages[receiver].get(sender) == None:
+                self.unreadMessages[receiver][sender] = 0
 
+            if(my_key in self.messages):
+                new_messages = self.messages[my_key].show()
 
+                while self.unreadMessages[sender][receiver] > 0:
+                    index = len(new_messages) - self.unreadMessages[sender][receiver]
+                    try:
+                        n = new_messages[index]
+                    except KeyError:
+                        continue
+
+                    if self.unreadMessages[sender][receiver] > 0:
+                        self.unreadMessages[sender][receiver] -= 1
+                    yield n
+            time.sleep(0.1)
+                
 if __name__ == '__main__':
-    port = 11912
-    # create a gRPC server
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     rpc.add_ChatServerServicer_to_server(ChatServer(), server)
-
-    print('Starting server. Listening...')
+    print('[Spartan] Connected to Spartan Server at port {}.'.format(port))
     server.add_insecure_port('[::]:' + str(port))
     server.start()
-    # Server starts in background (another thread) so keep waiting
+
     while True:
         time.sleep(64 * 64 * 100)
